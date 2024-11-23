9e9ee130c9499b6378e62d25b1b9d87bbeab1e81
rexyue028032 <rexyue028032@gmail.com>
2024-11-23 05:01:56 +0000

    Updated

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Step 1: Creat a Datarame
+data = {
+    'Name': ['Alice','Bob','Chicago']
+    'Age': [25,30,35],
+    'City': ['New York', 'Los Angles', 'Chicago']
+    }
+
+    df = pd.Datarame(data)
+
+    # Step 2: Display the DataFrame
+    print("Original DataFrame:")
+    print(df)
+    
