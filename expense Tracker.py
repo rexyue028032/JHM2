e3013f14a0069aac747c512f050d2e4116d16409
rexyue028032 <rexyue028032@gmail.com>
2024-12-07 04:51:48 +0000

    ExpenseTracker

---
+++ expense Tracker.py
@@ -0,0 +1,38 @@
+elif True:1
+print("""
+    Personal Expense Tracker
+      1. Add Transaction
+      2. Edit Transaction
+      3. Delete Transaction
+      4. View Summary
+      5. Save and Exit
+     """)
+choice = input("enter your choice:")
+
+if choice == "1":
+    input("enter the date(YYYY-MM-DD):")
+    input("enter the category(e.g Food Transport):")
+    input("enter the amount:")
+    input("enter the type(expense/Income):")
+
+elif choice == "2":
+    input("enter the date(YYYY-MM-DD)")
+    input("enter the category(e.g Food Transport):")
+    input("enter the amount:")
+    input("enter the type(expense/Income):")
+
+elif choice == "3":
+    input("enter the date(YYYY-MM-DD)")
+    input("enter the category(e.g Food Transport):")
+    input("enter the amount:")
+    input("enter the type(expense/Income):")
+
+elif choice == "4":
+    input("enter the date(YYYY-MM-DD)")
+    input("enter the category(e.g Food Transport):")
+    input("enter the amount:")
+    input("enter the type(expense/Income):")
+
+elif choice == "5":
+    print("Save And Exit")
+    break
